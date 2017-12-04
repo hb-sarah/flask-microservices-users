11bb60650e1c9aa9b5a77df68742eff2cabd36b2
Sarah Betack <sarah@thehelperbees.com>
2017-12-04 15:15:08 -0600

    complete part 2 lesson 3

--- manage.py
+++ manage.py
@@ -1,9 +1,19 @@
 import unittest
+import coverage
 
 from flask_script import Manager
 
 from project import create_app, db
 from project.api.models import User
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*'
+    ]
+)
+COV.start()
 
 app = create_app()
 manager = Manager(app)
@@ -18,6 +28,21 @@ def test():
     return 1
 
 @manager.command
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
+    
+@manager.command
 def recreate_db():
     """Recreates a database."""
     db.drop_all()
@@ -30,6 +55,7 @@ def seed_db():
     db.session.add(User(username='michael', email="michael@realpython.com"))
     db.session.add(User(username='michaelherman', email="michael@mherman.org"))
     db.session.commit()
+
 
 if __name__ == '__main__':
     manager.run()
